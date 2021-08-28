5dcf34fa4e8e8022d1584504d32b2047ae8986a2
vinod096 <vinod.raipati@hotmail.com>
2021-08-28 13:02:26 +1000

    using_in_command

---
+++ in_command.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+groceries = pd.Series(data=[30, 6, 'yes', 'no'], index=['eggs', 'apples', 'milk', 'bread'])
+
+print("Groceries : \n", groceries)
+
+x = 'banans' in groceries
+
+print('Is bananas an index label in Groceries:', x)
+
