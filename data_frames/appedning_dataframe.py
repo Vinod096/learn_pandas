568de6ee49f30a787b43bc95e8f001792d5a428b
vinod096 <vinod.raipati@hotmail.com>
2021-08-29 10:21:39 +1000

    appending_a_dataframes

---
+++ data_frames/appedning_dataframe.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+items = [{'bikes': 20, 'pants': 30, 'watches': 35}, {'watches': 10, 'glasses': 50, 'bikes': 15, 'pants': 5}]
+
+store_items = pd.DataFrame(items, index=['store 1', 'store 2'])
+
+print("store_items : \n", store_items)
+
+new_items = [{'bikes' : 30,'pants' : 40,'watches' : 50, 'glasses' : 60}]
+
+new_store = pd.DataFrame(new_items, index =['store 3'] )
+
+#print("New Items : ",new_items)
+
+store_items = store_items.append(new_store)
+
+print("New Store : \n", store_items)
