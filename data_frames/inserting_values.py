669dcdb6f01b6ee0b514e44d25894efb0b43f9ae
vinod096 <vinod.raipati@hotmail.com>
2021-08-29 10:45:39 +1000

    inserting_new_values_into_dataframe

---
+++ data_frames/inserting_values.py
@@ -0,0 +1,24 @@
+import pandas as pd
+items = [{'bikes': 20, 'pants': 30, 'watches': 35}, {'watches': 10, 'glasses': 50, 'bikes': 15, 'pants': 5}]
+
+store_items = pd.DataFrame(items, index=['store 1', 'store 2'])
+
+print("store_items 1 : \n", store_items)
+
+new_items = [{'bikes': 30, 'pants': 40, 'watches': 50, 'glasses': 60}]
+
+new_store = pd.DataFrame(new_items, index=['store 3'])
+
+#print("New Items : ",new_items)
+
+store_items = store_items.append(new_store)
+
+print("New Store 2 : \n", store_items)
+
+store_items['new_watches'] = store_items['watches'][1:]
+
+print("New store items : \n", store_items)
+
+store_items.insert(3,'shoes',[10,5,6])
+
+print("Store items 3 : \n",store_items)
